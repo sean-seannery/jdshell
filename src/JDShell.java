085913a883becb1cd38ec5c79922d1ff4a09122c
sean_seannery <im.sean@gmail.com>
2013-06-03 19:25:56 -0700

    Added the ability to target commands at 1 server

--- src/JDShell.java
+++ src/JDShell.java
@@ -3,7 +3,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -11,7 +13,7 @@ import java.util.Scanner;
  * This is the Java Distributed Shell Client.  It reads in commands and spins of JDShellClientThreads
  * which send the command to the peers and process the feedback. This is for a Operating Systems class
  * project.  I would have like to have written this shell in C++ but socket/network/multithreading is a
- * pain in the ass in c++ for only 10 weeks.
+ * pain in the ass in c++ for only 10 weeks.  
  * 
  * CAVEAT!  Unfortunately this shell does not currently support any xterm applications such as vim,
  * top, more, etcetera. 
@@ -21,7 +23,7 @@ import java.util.Scanner;
 public class JDShell {
 	
 	//file that gets read in and loads this computer's peers.  you may need to change this to get it to work
-	private static final String PEER_LIST_FILE_LOC = "./peer_list";
+	private static final String PEER_LIST_FILE_LOC = "../peer_list";
 	private static ArrayList<String> peers;
 	
 	//the current directory. cd is not an external program, so this gets changed when the user issues 'cd"
@@ -34,22 +36,34 @@ public class JDShell {
 	public static void main(String[] args) {
 		init();
 		currentDir = System.getProperty("user.dir");
+
 		//System.out.println(currentDir);
 		String input = null;
 		JDShellServer server = new JDShellServer();
 		server.start();
 		
 		Scanner in = new Scanner(System.in);
-		System.out.print("JDS%:");
+		showPrompt();
 		//stop the client if user enters "quit" or "exit"
 		while (!(input = in.nextLine()).equals("quit") && !input.equals("exit")){
 			
 			if (input.equals("")) {
-				System.out.print(currentDir + " - JDS%: ");
+				showPrompt();
 			} else {
+				
+				//handle the ability to target a command to only one server using !@peeraddress
+				//example "touch newfile.txt !@127.0.0.1"
+				ArrayList<String> tempPeers = peers;
+				if (input.contains("!@")){
+					String singleton = input.split("!@")[1];
+					input = input.split("!@")[0];
+					tempPeers = new ArrayList<String>();
+					tempPeers.add(singleton);
+				}
+				
 				//loop through all peers and spin off a new thread that handles the input
 				ArrayList<JDShellClientThread> threadList = new ArrayList<JDShellClientThread>();
-				for (String peer : peers){
+				for (String peer : tempPeers){
 					threadList.add(new JDShellClientThread(peer, input, currentDir));
 					threadList.get(threadList.size()-1).start();
 				}
@@ -78,7 +92,7 @@ public class JDShell {
 						currentDir = newDir;
 				}
 				
-				System.out.print(currentDir + " - JDS%: ");
+				showPrompt();
 			}
 			
 		}
@@ -86,6 +100,15 @@ public class JDShell {
 
 	}
 	
+	/**
+	 * Formats the appearance of the shell prompt and displays it.
+	 */
+	private static void showPrompt(){
+		String dir = new File(currentDir).getName();
+		SimpleDateFormat sdfDate = new SimpleDateFormat("hh:mm:ss.SSS");
+		Date now= new Date();
+		System.out.print("["+sdfDate.format(now)+"] "+ dir + " - JDS%: ");
+	}
 	
 	/**
 	 * Reads in the Peer List File and stores it to an array to send off to people
